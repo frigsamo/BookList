e287d4ea497fed86aa2b21779bd144920e7370a5
Frank Santana <frigsamo@gmail.com>
2020-02-11 15:58:40 -0500

    Updates

---
+++ precache-manifest.c9472a592a71565657a15647de149689.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54097ae9f63d981e712cd6d2d5a3194d",
+    "url": "/Booklist/index.html"
+  },
+  {
+    "revision": "c05b545d03aa17dbdc85",
+    "url": "/Booklist/static/css/main.f7e1efb5.chunk.css"
+  },
+  {
+    "revision": "3b8eb8cc91fbf5dd6da7",
+    "url": "/Booklist/static/js/2.552a88ed.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Booklist/static/js/2.552a88ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c05b545d03aa17dbdc85",
+    "url": "/Booklist/static/js/main.41631af2.chunk.js"
+  },
+  {
+    "revision": "02cf77b768dfc9360f4c",
+    "url": "/Booklist/static/js/runtime-main.220dffe1.js"
+  }
+]);
